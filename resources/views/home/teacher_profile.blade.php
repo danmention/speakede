8e635e4d73931282418f11fe6958dd98010f1dee
israel eghwrudjakpo <israel.eghwrudjako@capricorndigi.com>
2023-08-19 14:20:53 +0100

    UI modifications

--- resources/views/home/teacher_profile.blade.php
+++ resources/views/home/teacher_profile.blade.php
@@ -1,8 +1,6 @@
 @extends('home.template')
 @section('content')
 
-
-
     <!-- Team Profile Start -->
     <div class="section upstudy-team-profile-section section-padding">
         <div class="container">
@@ -15,24 +13,29 @@
                             <div class="team-profile-info">
 
                                 @foreach($profile as $row)
-                                <div class="instructor-profile">
+                                    <div class="instructor-profile">
 
 
                                         <div class="profile-images">
 
                                             @if(!empty($row->profile_image))
                                                 <div class="figure mb-3">
-                                                    <img src="{{asset('profile/photo/'.$row->id.'/'.$row->profile_image)}}" class="img-avatar" alt="author" style="width: 80px">
+                                                    <img
+                                                        src="{{asset('profile/photo/'.$row->id.'/'.$row->profile_image)}}"
+                                                        class="img-avatar" alt="author" style="width: 80px">
                                                 </div>
                                             @else
                                                 <div class="figure mb-3">
-                                                    <img src="{{asset('avater2.png')}}" class="img-avatar" alt="author" style="width: 80px">
+                                                    <img src="{{asset('avater2.png')}}" class="img-avatar" alt="author"
+                                                         style="width: 80px">
                                                 </div>
                                             @endif
 
                                         </div>
                                         <div class="profile-content">
-                                            <h5 class="name"><a href="{{url('teacher/'.$row->identity)}}">{{$row->firstname. ' '.$row->lastname}}</a> </h5>
+                                            <h5 class="name"><a
+                                                    href="{{url('teacher/'.$row->identity)}}">{{$row->firstname. ' '.$row->lastname}}</a>
+                                            </h5>
 
                                             <p>PROFESSIONAL TEACHER</p>
                                             <p>Teaches:</p>
@@ -43,7 +46,7 @@
 
                                         </div>
 
-                                </div>
+                                    </div>
 
                             </div>
                             <!-- Team Profile Info End -->
@@ -58,11 +61,12 @@
                             <div class="col-lg-10">
                                 <div class="team-profile-description">
                                     <h3 class="title"> Availability</h3>
-                                    <br />
+                                    <br/>
 
-                                    <div class="block-content block-content-full space-y-3 row" >
+                                    <div class="block-content block-content-full space-y-3 row">
                                         <div class="form-check form-block col-lg-6" id="private_class">
-                                            <input type="radio" class="form-check-input" id="checkout-delivery-1" name="checkout-delivery" checked="">
+                                            <input type="radio" class="form-check-input" id="checkout-delivery-1"
+                                                   name="checkout-delivery" checked="">
                                             <label class="form-check-label" for="checkout-delivery-1">
                                             <span class="d-block fw-normal p-1">
                                               <span class="d-block fw-semibold mb-1">Private Class</span>
@@ -72,9 +76,10 @@
                                         </div>
 
                                         @if($private_class->count() > 0)
-                                        <div class="form-check form-block col-lg-6" id="group_class">
-                                            <input type="radio" class="form-check-input" id="checkout-delivery-2" name="checkout-delivery">
-                                            <label class="form-check-label" for="checkout-delivery-2">
+                                            <div class="form-check form-block col-lg-6" id="group_class">
+                                                <input type="radio" class="form-check-input" id="checkout-delivery-2"
+                                                       name="checkout-delivery">
+                                                <label class="form-check-label" for="checkout-delivery-2">
                                                 <span class="d-block fw-normal p-1">
                                                   <span class="d-block fw-semibold mb-1">
                                                     Group Class
@@ -82,11 +87,11 @@
                                                   </span>
                                                   <span class="d-block fs-sm fw-medium text-muted">{{$private_class[0]->available_slots}} Slot available</span>
                                                 </span>
-                                            </label>
-                                        </div >
+                                                </label>
+                                            </div>
                                         @endif
                                     </div>
-                                    <br />
+                                    <br/>
                                 </div>
                             </div>
 
@@ -102,27 +107,31 @@
                             <div class="col-lg-10" id="group_class_view" style="display: none;">
                                 <!-- Team Profile Start -->
 
-                                @if($private_class[0]->available_slots == 0)
-                                    <div class="group_class">
-                                        <div class="image-text">
-                                            <h2>Group Class is no longer Available</h2>
+                                @if(!empty($private_class[0]))
+                                    @if($private_class[0]->available_slots == 0)
+                                        <div class="group_class">
+                                            <div class="image-text">
+                                                <h2>Group Class is no longer Available</h2>
+                                            </div>
                                         </div>
-                                    </div>
-                                @else
-                                <div class="group_class">
-                                    <div class="image-text">
-                                        <h3 class="number">{{$private_class[0]->title}}</h3>
-                                        <p>{!! $private_class[0]->description !!}</p>
+                                    @else
+                                        <div class="group_class">
+                                            <div class="image-text">
+                                                <h3 class="number">{{$private_class[0]->title}}</h3>
+                                                <p>{!! $private_class[0]->description !!}</p>
 
-                                        <h4>Price : ₦{{ number_format($private_class[0]->price)}}</h4>
+                                                <h4>Price : ₦{{ number_format($private_class[0]->price)}}</h4>
 
-                                        <a href="{{ url('user/apply/booking/group/lesson/pay?teacher_id='.$identity.'&slot='.$private_class[0]->slot.'&id='.$private_class[0]->id) }}" style="margin-top: 0px !important;background: #e1ac4b;color: #ffffff;padding: 8px;border-radius:6px;" class="ml-2">
-                                            <strong>Book Now</strong>
-                                        </a>
-                                    </div>
-                                </div>
+                                                <a href="{{ url('user/apply/booking/group/lesson/pay?teacher_id='.$identity.'&slot='.$private_class[0]->slot.'&id='.$private_class[0]->id) }}"
+                                                   style="margin-top: 0px !important;background: #e1ac4b;color: #ffffff;padding: 8px;border-radius:6px;"
+                                                   class="ml-2">
+                                                    <strong>Book Now</strong>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    @endif
                                 @endif
-                                <!-- Team Profile End -->
+                                    <!-- Team Profile End -->
                             </div>
 
                         </div>
